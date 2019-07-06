0c1e05f6030051205c4011ba0f85ddc602d62d07
chano <chano22@naver.com>
2019-07-07 02:18:19 +0900

    grid examples succes

--- thymleafgrid/src/main/java/com/grid/main/controllers/GridController.java
+++ thymleafgrid/src/main/java/com/grid/main/controllers/GridController.java
@@ -9,6 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.grid.main.entity.User;
 import com.grid.main.repository.UserRepository;
+import com.grid.main.util.ReflectioinUtils;
 
 @Controller
 public class GridController {
@@ -21,13 +22,13 @@ public class GridController {
     	System.out.println("test");
     	
     	User user=new User();
-		user.setAge("15");
-		user.setName("ㅇ오우민");
+		user.setAge("16");
+		user.setName("testname1");
 		userRepository.save(user);
 
     	User user2=new User();
-		user2.setAge("16");
-		user2.setName("ㅇ그네그네");
+		user2.setAge("15");
+		user2.setName("testname2");
 		userRepository.save(user);
 
 		
@@ -35,7 +36,7 @@ public class GridController {
    
     	ModelAndView mv = new ModelAndView("grid");
         mv.addObject("gridlist", userRepository.findAll());
-        mv.addObject("gridlist", userRepository.findAll());
+        mv.addObject("NameList", ReflectioinUtils.ClassProperties(User.class));
         
         return mv;
     }
